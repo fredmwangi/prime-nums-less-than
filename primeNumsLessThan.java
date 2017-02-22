7a6a9633f5c47d0cf236833074416f03276a3c08
Frederick Mwangi <mwangi.frederick@gmail.com>
2017-02-22 15:11:44 -0500

    Ask user for an integer
    Ask the user for an integer in order to calculate all of the prime
    numbers less than that integer.

--- primeNumsLessThan.java
+++ primeNumsLessThan.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class primeNumsLessThan
 {
@@ -8,10 +9,14 @@ public class primeNumsLessThan
     int Prime;
     List<Integer> list = new ArrayList<>();
     boolean prime;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter an integer: ");
+    int number = scanner.nextInt();
     
-    for(int i=2;i<100;i++)
+    for(int i=2;i<number;i++)
     {
-    	prime = true;  
+    	prime = true;
       	for(int k=2;k<i;k++)
         {
         	if(i%k==0)
@@ -23,7 +28,7 @@ public class primeNumsLessThan
       	if(prime==true)
         {
       		list.add(i);
-      	}      
+      	}
     }
     System.out.println(list);
   }
