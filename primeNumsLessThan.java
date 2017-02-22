0285ba0727373d836ad4a1660f9ee7d8a4608b80
Frederick Mwangi <mwangi.frederick@gmail.com>
2017-02-22 13:04:09 -0500

    Create primeNumsLessThan.java

---
+++ primeNumsLessThan.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+  	int Prime;
+  	List<Integer> list = new ArrayList<>();
+  	boolean prime;
+    
+    for(int i=2;i<100;i++)
+    {
+    	prime = true;
+    	
+      	for(int k=2;k<i;k++)
+        {
+        	if(i%k==0)
+            {
+            	prime = false;
+            	break;
+        	}
+        }
+      	
+      	if(prime==true)
+        {
+      		list.add(i);
+      	}      
+    }
+    System.out.println(list);
+  }
+}
