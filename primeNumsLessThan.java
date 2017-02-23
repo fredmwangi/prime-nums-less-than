98aaccd3191414b9e1c61722c740b4de803153b7
Frederick Mwangi <mwangi.frederick@gmail.com>
2017-02-23 12:40:35 -0500

    Update primeNumsLessThan.java

--- primeNumsLessThan.java
+++ primeNumsLessThan.java
@@ -20,7 +20,7 @@ public class primeNumsLessThan
       	for(int k=2;k<i;k++)
         {
         	if(i%k==0)
-              {
+                {
             	prime = false;
             	break;
         	}
