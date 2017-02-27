206ffd0ab6cdcd008dce40a954e4a91caf7673eb
Frederick Mwangi <mwangi.frederick@gmail.com>
2017-02-27 05:34:11 -0500

    Update primeNumsLessThan.c

--- primeNumsLessThan.c
+++ primeNumsLessThan.c
@@ -1,11 +1,48 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 int main( ) {
   int number;
+  int arraypos = 0;
+  bool prime;
+  int *primes;
 
-  printf("Enter an Integer: ");
+  printf("Enter an integer: ");
   scanf("%d", &number);
 
-  printf("You entered: %d\n", number);
+  primes = (int *)malloc(number * sizeof(int));
+  if (NULL == primes) {
+  fprintf(stderr, "malloc failed\n");
+  return(-1);
+  }
 
+  for (int i = 2; i < number; i++) {
+    prime = true;
+    for (int k = 2; k < i; k++) {
+      if (i%k == 0) {
+        prime = false;
+        break;
+      }
+    }
+    if (prime == true) {
+      primes[arraypos] = i;
+      arraypos++;
+    }
+  }
+
+  for (int i = 0; i < number; i++) {
+    if (primes[i] == 0) {
+      break;
+    }
+    if (i == 0) {
+      printf("%d" ,primes[i]);
+    }
+    else{
+      printf(", %d" ,primes[i]);
+    }
+  }
+
+  free((int*) primes);
   return 0;
 }
